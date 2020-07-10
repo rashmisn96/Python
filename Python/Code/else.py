9fd620b4d96d7eb2bbb45c4bc9e3a9c5f823ccb5
Rashmi S N <rashmi.sn@actcorp.local>
2020-07-11 00:33:16 +0530

    h

---
+++ Python/Code/else.py
@@ -0,0 +1,13 @@
+a=10
+b=10
+if b > a:
+     print('b greater than a')
+elif a == b:
+    print('both are equal')
+else:
+    print('a greater than b')
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+       
